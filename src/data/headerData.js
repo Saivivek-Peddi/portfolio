d8dcb026c75e5fd8ab572985af16d99bb745b1e8
Saivivek-Peddi <peddisaivivek999@gmail.com>
2023-12-11 22:49:47 -0800

    Added github pages code

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import profileImg from '../assets/png/profileImage.JPG'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    name: 'Sai Vivek Peddi',
+    title: "AI Researcher",
+    desciption:"Focused on the synergy of software and hardware in AI, I specialize in hardware accelerators for Large Language Models. My journey blends academic insights in Computer Science with practical industry experiences, driving my passion for enhancing AI through innovative hardware advancements.",
+    image: profileImg,
     resumePdf: resume
 }
